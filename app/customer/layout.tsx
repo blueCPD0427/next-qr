e9a8e33ab1f733bfeb6e9d784bb2a664f3d59d46
nakahara <nakahara@mandl.co.jp>
2024-08-28 09:05:39 +0900

    オーナーページ：QRコード読み取りページの構成修正。 共通：ヘッダーのサイト名でトップに戻れるように変更

--- app/customer/layout.tsx
+++ app/customer/layout.tsx
@@ -4,6 +4,7 @@ import { faQrcode } from "@fortawesome/free-solid-svg-icons";
 import { Dela_Gothic_One } from "next/font/google";
 import { LogoutForm } from "@/app/lib/component-parts/buttons";
 import { auth } from "@/auth";
+import Link from "next/link";
 
 const dela = Dela_Gothic_One({
     subsets: ["latin"],
@@ -21,11 +22,11 @@ export default async function CustomerLayout({
             <div>
                 <header className="fixed top-0 left-0 w-full shadow-md z-50">
                     <div className="bg-blue-200 px-5 py-2 flex justify-between">
-                        <h1 className={`${dela.className} text-lg`}>
+                        <Link href={"/customer/menu"} className={`${dela.className} text-lg`}>
                             <FontAwesomeIcon icon={faQrcode} />
                             QR君!!-会員サイト-
                             <FontAwesomeIcon icon={faQrcode} />
-                        </h1>
+                        </Link>
                         {
                             session != null && session.user?.type == 'customer'
                             &&
